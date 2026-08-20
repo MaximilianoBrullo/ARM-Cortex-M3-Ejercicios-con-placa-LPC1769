bc7fae298ec1aa84e42f6ff6b1e9643eb07d18f5
Maximliano Brullo <maximilianobrullo4@gmail.com>
2026-08-20 12:26:49 -0300

    Correciones de holaMundo.c

--- Unidad 1 GPIO/HolaMunido.c
+++ Unidad 1 GPIO/HolaMunido.c
@@ -1,43 +1,64 @@
-#include "LPC17XX.h";
+/*
+ * Directivas de inclusion
+ */
+#include "LPC17xx.h"
 
-#define PINSEL_LED = ~(3<<0)
-#define PINMODE_LED = ~(2<<0)
+/*
+ * Definiciones y Macros
+ */
+#define LED (1<<22)
 
+/*
+ * Prototipos de funciones
+ */
+void configPCB(void);
+void delay(void);
+void blinkLED(void);
 
-void configPCB();
-void blinkLED();
-void delay();
-
-int main(){
-    configPCB();
-
-    blinkLED();
-}
-
-void configPCB(){
-    //CONFIGURO EL PIN 0 DEL PUERTO 0
-    LPC_PINCON -> PINSEL(0) &= PINSEL_LED; //PINSEL0[1:0] = 11
-    //LPC_GPIO0 -> PINMODE(0) &= PINMODE_LED; //PINMODE0[1:0] = 10
-    LPC_GPIO0 -> FIODIR |= (1<<0); //F
-}
-
-void blinkLED(){
-    while(1){
-        LPC_GPIO0 -> FIOSET |= (1<<0); //ENCENDER LED
-        delay(); //DELAY
-        LPC_GPIO0 -> FIOCLR |= (1<<0); //APAGAR LED
-        delay(); //DELAY
-    }
+int main(void){
+	configPCB();
+	while(1){
+		blinkLed();
+		delay();
+	}
 }
 
 /*
-@brief: Function to generate a delay
-@detailed: This function generates a delay by executing a loop that runs for a specified number of iterations. The __NOP instruction is used to create a no-operation delay, allowing the processor to wait without performing any other operations. The delay duration can be adjusted by changing the loop count.
+ * @brief Configura el pin P0.22 como salida digital
+ *
+ * @details Selecciona la funcion GPIO para p0.22, configura el pin como
+ * 			salida digital y habilita su acceso mediante el registro FIOMASK
+ */
+void configPCB(void){
+	LPC_PINCON->PINSEL1 &= ~(0b11<<12);//p0.22 GPIO Function
+	//no utilizamos pinMode ya que es solo para cuando usamos el pin como entrada
+	LPC_GPIO0->FIODIR |= (1<<22);//p0.22 Como salida al poner 1 logico en el pin 22
+	LPC_GPIO0->FIOMASK |= (1<<22);//coloco un un 1 logicoara que no cambie el estado de FIO set,clr,pin
+	return;
+}
 
-*/
-
+/*
+ * @brief Genera un retardo bloqueante mediante instruciones NOP.
+ *
+ * @details La espera se ejecuta mediante un ciclo for y el tiempo depende de la frecuencia del CPU y
+ * 			de la cantidad de
+ */
 void delay(){
-    for(int i=0; i<1000000; i++){
-        __NOP;
-    } //DELAY
+	for(int i = 0; i<4000000; i++){
+		__NOP();
+	}
+}
+
+/*
+ * @brief Cambia el estado del pin, de uno a cero y de cero a uno haciendo l lectura del pin mediante
+ * 		  mediante el registro FIOSET.
+ * @details Leemos el estado del pin 22 en el puerto 0, si esta encendido lo apagamos, si esta apagado lo encendemos
+ */
+void blinkLed(){
+	uint32_t estadoPin = (LPC_GPIO0->FIOSET & (1<<22));
+	if(estadoPin == 0){
+		LPC_GPIO0->FIOSET |= (1<<22);
+	}else{
+		LPC_GPIO0->FIOCLR |= (1<<22);
+	}
 }
